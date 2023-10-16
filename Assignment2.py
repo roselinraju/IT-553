b197693755262480455edda066188f25bc9fc638
roselinraju <146775197+roselinraju@users.noreply.github.com>
2023-10-16 17:17:06 -0400

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,91 @@
+import math
+
+first_name = "matt"
+LAST_NAME =  "HENDRICKSON"
+
+print('Hello,',first_name.upper(),LAST_NAME.lower())
+
+#Code till part 3
+
+print("\n\n")
+
+new_var = first_name + " " + LAST_NAME
+print(new_var[:len(new_var)-len(LAST_NAME)])
+
+#code till part 6
+new_var = new_var.replace(LAST_NAME,"Walsh College Student")
+print(new_var)
+
+print('''"Start by doing what's necessary; then do what's possible; and sudeenly you are doing the impossible - Francis of Assisi"''')
+
+#code till part 8
+
+deci_one = 10.0
+deci_two = 20.0
+
+add = deci_two + deci_one
+sub = deci_two - deci_one
+multi = deci_one * deci_two
+div = deci_two / deci_one
+
+print(deci_one,'plus',deci_two,'equals',add)
+print(str(deci_two)+" minus "+str(deci_one)+" equals "+str(sub))
+print(deci_one,'multiplication',deci_two,'equals',multi)
+print(str(deci_two)+" division "+str(deci_one)+" equals "+str(div))
+
+#code till part 11
+
+sq_root = math.sqrt(multi)
+print('The square root of',multi,'equals',sq_root)
+
+#code til part 12
+
+curr_month = "September"
+day = 26
+print("\t\tToday is day "+str(day)+" of the month of "+curr_month)
+import math
+
+first_name = "matt"
+LAST_NAME =  "HENDRICKSON"
+
+print('Hello,',first_name.upper(),LAST_NAME.lower())
+
+#Code till part 3
+
+print("\n\n")
+
+new_var = first_name + " " + LAST_NAME
+print(new_var[:len(new_var)-len(LAST_NAME)])
+
+#code till part 6
+
+new_var = new_var.replace(LAST_NAME,"Walsh College Student")
+print(new_var)
+
+print('''"Start by doing what's necessary; then do what's possible; and sudeenly you are doing the impossible - Francis of Assisi"''')
+
+#code till part 8
+
+deci_one = 10.0
+deci_two = 20.0
+
+add = deci_two + deci_one
+sub = deci_two - deci_one
+multi = deci_one * deci_two
+div = deci_two / deci_one
+
+print(deci_one,'plus',deci_two,'equals',add)
+print(str(deci_two)+" minus "+str(deci_one)+" equals "+str(sub))
+print(deci_one,'multiplication',deci_two,'equals',multi)
+print(str(deci_two)+" division "+str(deci_one)+" equals "+str(div))
+
+#code till part 11
+
+sq_root = math.sqrt(multi)
+print('The square root of',multi,'equals',sq_root)
+
+#code til part 12
+
+curr_month = "September"
+day = 26
+print("\t\tToday is day "+str(day)+" of the month of "+curr_month)
